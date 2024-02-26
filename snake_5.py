3a13114256dda8f03b0b3e78babb1e301cb42953
ZV-96 <98915389+ZV-96@users.noreply.github.com>
2024-02-27 12:04:45 +1300

    update

--- snake_5.py
+++ snake_5.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 
@@ -8,7 +9,7 @@ pygame.display.set_icon(game_icon)
 pygame.display.set_caption("Snake Game - by Zack van Rooyen")
 
 # Tuples containing the colours to be used in the game
-Black = (0, 0, 0)
+black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 51, 51)
 green = (102, 255, 178)
@@ -17,18 +18,28 @@ teal = (0, 153, 153)
 # Fonts for the game
 score_font = pygame.font.SysFont("comicsans", 20)
 exit_font = pygame.font.Font("freesansbold.ttf", 30)
+msg_font = pygame.font.SysFont("comicsans", 50)
+
+
+def message(msg, txt_colour, bkgd_colour):
+    txt = msg_font.render(msg, True, txt_colour, bkgd_colour)
+
+    # Centre rectangle: 1000/2 = 500 and 650/2 = 325
+    text_box = txt.get_rect(center=(500, 325))
+    screen.blit(txt, text_box)
+
 
 # Sets the speed at which the snake moves
 clock = pygame.time.Clock()
 
 quit_game = False
 
-# Snake will be 20 by 20 pixels
-snake_x = 490  # Centre point horizontally
-snake_y = 315  # Centre point vertically
+# Snake will be 20 x 20 pixels
+snake_x = 490
+snake_y = 315
 
-snake_x_change = 0 # holds the value change of the x co ordinant
-snake_y_change = 0 # holds the value change of the y co ordinant
+snake_x_change = 0
+snake_y_change = 0
 
 # Loop to keep screen open until user presses x
 while not quit_game:
@@ -36,7 +47,7 @@ while not quit_game:
         if event.type == pygame.QUIT:
             quit_game = True
 
-        if event.type == pygame. KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 snake_x_change = -20
                 snake_y_change = 0
@@ -50,18 +61,24 @@ while not quit_game:
                 snake_x_change = 0
                 snake_y_change = 20
 
+    if snake_x >= 1000 or snake_x < 0 or snake_y >= 650 or snake_y < 0:
+        quit_game = True
+
     snake_x += snake_x_change
     snake_y += snake_y_change
 
-    # Changes background to green
-    screen.fill(Black)
+    screen.fill(black)  # Changes background to green
 
     # Create the snake body (rectangle)
     pygame.draw.rect(screen, teal, [snake_x, snake_y, 20, 20])
     pygame.display.update()
 
-    # sets the speed which each iteration of the game
-    clock.tick(5)
+    clock.tick(5)  # sets the speed which each iteration of the game
+    # runs the in frames per second
+
+message("You suck", teal, black)
+pygame.display.update()
+time.sleep(3)
 
 pygame.quit()
 quit()
